e61f0c8de072c914aa6959784c62fc5139bdfd79
Ariel Patiño <ariel.patino@gmail.com>
2025-08-20 17:26:01 -0500

    feat: Add WebProcessor
     Changes to be committed:
    	modified:   src/EndpointLoggerServiceProvider.php

--- src/EndpointLoggerServiceProvider.php
+++ src/EndpointLoggerServiceProvider.php
@@ -3,6 +3,7 @@
 namespace ArielBlackymetal\EndpointLogger;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Processor\WebProcessor;
 use Illuminate\Support\Facades\Log;
 use ArielBlackymetal\EndpointLogger\Logging\Handler\EndpointHandler;
 use Monolog\Logger;
@@ -16,9 +17,11 @@ class EndpointLoggerServiceProvider extends ServiceProvider
             $url = $config['url'] ?? null;
             $level = $config['level'] ?? 'debug';
 
-            return new Logger('custom-log', [
-                new EndpointHandler($url, $appName, $level)
-            ]);
+            $handler = new EndpointHandler($url, $appName, $level);
+
+            $handler->pushProcessor(new WebProcessor());
+
+            return new Logger('custom-log', [$handler]);
         });
     }
 }
